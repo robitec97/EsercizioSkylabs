67da3e71f3f3d5d74fded18d2d69069bc855cb3d
robitec97 <45318540+robitec97@users.noreply.github.com>
2021-10-11 01:56:20 +0200

    Aggiunto download file csv
    La route /records fa il download in csv di tutti i record denormalizzati

--- EsercizioSkylabs/Controllers/RecordsController.cs
+++ EsercizioSkylabs/Controllers/RecordsController.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using EsercizioSkylabs.Repositories;
 using EsercizioSkylabs.Models;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
 namespace EsercizioSkylabs.Controllers
 {
     [Route("api/records")]
@@ -24,5 +28,24 @@ namespace EsercizioSkylabs.Controllers
         {
             return await _repository.GetRecords(offset, count);
         }
+
+        [HttpGet]
+        public ActionResult Download()
+        {
+            List<RecordsDenormalized> recordsToDownload = (List<RecordsDenormalized>)_repository.GetAllRecords();
+            var cc = new CsvConfiguration(new System.Globalization.CultureInfo("en-US"));
+            using (var ms = new MemoryStream())
+            {
+                using (var sw = new StreamWriter(stream: ms, encoding: System.Text.Encoding.UTF8))
+                {
+                    using(var cw = new CsvWriter(sw, cc))
+                    {
+                        cw.WriteRecords(recordsToDownload);
+                    }
+                    return File(ms.ToArray(), "text/csv", $"export_{DateTime.UtcNow.Ticks}.csv");
+                }
+            }
+
+            }
     }
 }
