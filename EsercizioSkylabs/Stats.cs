dfcf355923da4e98886a68b670d352cb27cb805e
robitec97 <45318540+robitec97@users.noreply.github.com>
2021-10-11 14:58:16 +0200

    missing Stats
    uno dei 3 file mancanti come ho comunicato via mail

---
+++ EsercizioSkylabs/Stats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EsercizioSkylabs
+{
+    public class Stats
+    {
+        public string aggregationType { get; set; }
+        public int aggregationFilter { get; set; }
+        public float capital_gain_sum { get; set; }
+        public float capital_gain_avg { get; set; }
+        public float capital_loss_sum { get; set; }
+        public float capital_loss_avg { get; set; }
+        public int over_50k_count { get; set; }
+        public int under_50k_count { get; set; }
+
+
+    }
+}
